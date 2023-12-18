84faf9570ef20859c43b7f6bd611aee6749394e7
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-12-19 00:33:24 +0530

    Properly Implemented Cursor Follower

--- src/Components/GradientFollowCursor.tsx
+++ src/Components/GradientFollowCursor.tsx
@@ -1,41 +1,30 @@
-"use client"
+"use client";
 
 // components/MotionGradientFollowCursor.js
-import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from "react";
 
 const MotionGradientFollowCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  const handleMouseMove = (e: { clientX: any; clientY: any; }) => {
+  const handleMouseMove = (e: { clientX: any; clientY: any }) => {
     setPosition({ x: e.clientX, y: e.clientY });
   };
 
   useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener("mousemove", handleMouseMove);
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
-
   return (
-    <motion.div
-      className="fixed top-0 left-0 w-screen h-screen pointer-events-none"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 100 }}
-
-    >
-      <motion.div
-        className="absolute w-16 h-16 rounded-full bg-blue-950"
-        style={{
-          background: `radial-gradient(circle at 50% 50%, transparent, rgba(29, 78, 216, 0.05) 100%)`,
-          translateX: position.x-25,
-          translateY: position.y-25,
-        }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 10 }}
-      />
-    </motion.div>
+    <div
+      className={
+        "pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
+      }
+      style={{
+        background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+      }}
+    />
   );
 };
 
